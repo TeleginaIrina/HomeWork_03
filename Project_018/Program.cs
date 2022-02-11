609f82dbc3000004433cc465e693f4649e78998a
Телегина Ирина <ikoska056@gmail.com>
2022-02-11 17:44:43 +0300

    HomeWork_3

---
+++ Project_018/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 18: Проверить истинность утверждения    ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+// ¬ - знак отрицания
+// ⋀ - логическое "И"
+// ⋁ - логическое "ИЛИ"
+// следовательно не (X или Y) = не X и не Y
+
+bool x = true;
+bool y = true;
+
+if (!(x || y) == (!x && !y))
+    Console.WriteLine($"Вар. 1 утверждение истинно при x = true y = true");
+else
+    Console.WriteLine($"Вар. 1 утверждение не истинно при x = true y = true");
+
+x = true;
+y = false;
+
+if (!(x || y) == (!x && !y))
+    Console.WriteLine($"Вар. 2 утверждение истинно при x = true y = false");
+else
+    Console.WriteLine($"Вар. 2 утверждение не истинно при x = true y = false");
+
+x = false;
+y = true;
+
+if (!(x || y) == (!x && !y))
+    Console.WriteLine($"Вар. 3 утверждение истинно при x = false y = true");
+else
+    Console.WriteLine($"Вар. 3 утверждение не истинно при x = false y = true");
+
+x = false;
+y = false;
+
+if (!(x || y) == (!x && !y))
+    Console.WriteLine($"Вар. 4 утверждение истинно при x = false y = false");
+else
+    Console.WriteLine($"Вар. 4 утверждение не истинно при x = false y = false");
---
+++ Project_020/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+
+int x = new Random().Next(1, 5);
+Console.WriteLine(x);
+
+
+if (x == 1)
+    Console.WriteLine("Четверть I:  X=(0, +oo)  Y=(0, +oo)");
+if (x == 2)
+    Console.WriteLine("Четверть II:  X=(-oo, 0)  Y=(0, +oo)");
+if (x == 3)
+    Console.WriteLine("Четверть III:  X=(-oo, 0)  Y=(-oo, 0)");
+if (x == 4)
+    Console.WriteLine("Четверть IV:  X=(0, +oo)  Y=(-oo, 0)");
